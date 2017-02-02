b42d59cd2aea2bc7344dff0eceae9881e8a17fd0
Julian Trinh <julest@bu.edu>
2017-02-01 19:48:12 -0500

    Trying multiple techniques on video, no avail

--- Droplet_Characterization/Characterization_Dev/VulcanParseAndFormatting/CharacterizationInputParsing.py
+++ Droplet_Characterization/Characterization_Dev/VulcanParseAndFormatting/CharacterizationInputParsing.py
@@ -1,6 +1,7 @@
 # File for formatted argument input to the Characterization program.
 # Create instance of the class and access input elements with obj.args["<argument>"]
 import argparse
+import argcomplete
 
 class CharacterizationInputParsing:
 	def __init__(self):
@@ -10,6 +11,7 @@ class CharacterizationInputParsing:
 		self.parsing.add_argument("-f", "--format", action = "append",  help = "Formatting for the output, use -f for each section. E.g: -f option1 -f option2 -f option3")
 
 		self.args = vars(self.parsing.parse_args())
+		argcomplete.autocomplete(self.parsing)
 
 	def getFormat():
 		return self.args["format"]
