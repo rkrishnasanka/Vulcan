ffe422cc22b7a11f0b3050fe3786bb313fae138e
Julian Trinh <julest@bu.edu>
2017-02-03 22:18:42 -0500

    Drawing convex hulls, still seeing some disconnected regions, will need to look into combining close contours

--- Droplet_Characterization/Characterization_Dev/multiDroplet/VideoProcessing.py
+++ Droplet_Characterization/Characterization_Dev/multiDroplet/VideoProcessing.py
@@ -35,12 +35,12 @@ while (video.isOpened()):
 #		frame1Copy = bgSub.apply(frame1Copy)
 	
 #		diffFrame = cv2.absdiff(frame1Copy, frame2Copy)
-#		diffFrame = cv2.Canny(frame1Copy, 25, 100)
+		diffFrame = cv2.Canny(frame1Copy, 25, 100)
 
 #		ret, frame1Copy = cv2.threshold(frame1Copy, 100, 255, cv2.THRESH_BINARY_INV)		
 		frame1Copy = cv2.adaptiveThreshold(frame1Copy.copy(), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
-	#	frame1Copy = cv2.erode(frame1Copy, None, iterations=1)
-	#	frame1Copy = cv2.dilate(frame1Copy, None, iterations=2)
+#		frame1Copy = cv2.erode(frame1Copy, None, iterations=1)
+#		frame1Copy = cv2.dilate(frame1Copy, None, iterations=2)
 		im2, contours, hierarchy = cv2.findContours(frame1Copy.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 		if len(contours) > 0:
@@ -50,13 +50,13 @@ while (video.isOpened()):
 
 			for hull in hulls:
 				cv2.fillConvexPoly(frame1Copy, hull, (255,255,255), lineType=8, shift=0)
-				
+
 #				x,y,w,h = cv2.boundingRect(hull)
 #				cv2.rectangle(frame1Copy, (x,y), (x+w,y+h), (255,255,255),1)
 #			for contour in contours:
 #				hulls.append(cv2.convexHull(contour))
 
-#			cv2.drawContours(frame1Copy, hulls[0], -1 (255,255,255), -1)
+			#cv2.drawContours(frame1, hulls[0], -1 (255,255,255), -1)
 	
 		cv2.imshow("frame", frame1Copy)
 		if cv2.waitKey(1) & 0xFF == ord('q'):
