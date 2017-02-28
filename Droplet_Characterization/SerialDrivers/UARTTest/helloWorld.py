054ee47e1f47405597fd35f61cd43ae8d1be2796
Julian Trinh <julest@bu.edu>
2017-02-28 18:24:30 +0000

    Successful serial loopback test

---
+++ Droplet_Characterization/SerialDrivers/UARTTest/helloWorld.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import serial
+
+ser = serial.Serial(
+  port='/dev/ttyS0',
+  baudrate = 115200,
+  parity=serial.PARITY_NONE,
+  stopbits=serial.STOPBITS_ONE,
+  bytesize=serial.EIGHTBITS,
+  timeout=1
+)
+
+print "Serial is open: " + str(ser.isOpen())
+
+print "Now Writing"
+ser.write("This is a test")
+
+print "Did write, now read"
+x = ser.readline()
+print "got '" + x + "'"
+
+ser.close()
